1852ef22738b3706fe0a89112ad39c539b7f22e5
Alberto <albertojunior848423803@gmail.com>
2024-11-10 18:31:39 +0300

    changes

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,20 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.pritnln("Hello World");
+        // TODO code application logic here
+// create object of class LinkedList
+        LinkedList list = new LinkedList();
+        list.insert(10);
+        list.insert(20);
+        list.insert(30);
+        list.insertAtStart(90);
+        list.insertAtIndex(3,67);
+        list.insert (9);
+        System. out.println ("List before deletion:");
+        list.traverse();
+
+        list.deleteAtIndext(2);
+        System. out.println ("List after deletion:");
+        list.traverse();
     }
 }
---
+++ src/Node.java
@@ -0,0 +1,5 @@
+public class Node{
+    // declare the data and the link
+    int data;
+    Node next; // link
+}
